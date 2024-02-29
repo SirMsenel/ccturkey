4c59aae98801d59d131f621fa22c37809a36b2a1
mehmet şenel <senelm263@gmail.com>
2024-03-01 00:09:47 +0300

    basehdml

--- anaweb/urls.py
+++ anaweb/urls.py
@@ -6,6 +6,5 @@ app_name = 'anaweb'
 urlpatterns = [
     path("",views.home, name='home' ),
     path("Anasayfa/",views.Anasayfa, name='Anasayfa'),
-    path("Anasayfa/hakkimizda/",views.Anasayfa, name='hakkimizda')
 
 ]
